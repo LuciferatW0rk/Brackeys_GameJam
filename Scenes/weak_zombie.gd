8241fac6a4c2e73bc79de5df600c9b579d6c2ca7
Aditya Kumar Mishra <adityakr159@gmail.com>
2024-09-10 23:49:13 +0530

    zombie pathfinding

---
+++ Scenes/weak_zombie.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+var player = null
+
+const SPEED = 2.0
+
+@export var player_path : NodePath
+@onready var nav_agent = $NavigationAgent3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_node(player_path)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector3.ZERO
+	nav_agent.set_target_position(player.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	move_and_slide()

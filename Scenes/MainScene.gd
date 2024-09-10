9fe5df282f206c3a7a9b45342dd06af757c7c611
Aditya Kumar Mishra <adityakr159@gmail.com>
2024-09-10 23:16:09 +0530

    getting mobs

--- Scenes/MainScene.gd
+++ Scenes/MainScene.gd
@@ -1,5 +1,1 @@
 extends Node
-
-@onready var player = $CharacterBody3D  # Adjust the path if necessary
-@onready var grass_floor = $GrassFloor  # Adjust the path if necessary
-@onready var multimesh_instance = grass_floor.get_node("MultiMeshInstance3D")  # Adjust the path if necessary
